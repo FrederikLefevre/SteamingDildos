6b20b3dc3992b5b6b66af8c48dbe2065e9c5c4ce
Frederik <frederik.lefevre@outlook.com>
2016-03-01 20:23:50 +0100

    Added base game

--- FlightGame/MainWindow.xaml.cs
+++ FlightGame/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FlightGame
 {
@@ -20,9 +9,47 @@ namespace FlightGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Fields
+        //=======
+        private bool _planeTaken = false;
+
+        //Constructors
+        //=============
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Events
+        //=======
+        private void GrabPlane_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this._planeTaken = true;
+        }
+
+        private void MovePlane_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_planeTaken)
+            {
+                return;
+            }
+
+            Point mousePos = Mouse.GetPosition(PlayArea);
+
+            double xPos = mousePos.X - (this.Plane.ActualWidth / 2);
+            //double yPos = mousePos.Y - (this.Plane.ActualHeight / 2);
+
+            //Canvas.SetTop(this.Plane, yPos);
+            Canvas.SetLeft(this.Plane, xPos);
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            double startHeight = this.PlayArea.ActualHeight - (this.Plane.ActualHeight + 30);
+            double startWidth = (this.PlayArea.ActualWidth / 2) - (this.Plane.ActualWidth / 2);
+
+            Canvas.SetTop(this.Plane, startHeight);
+            Canvas.SetLeft(this.Plane, startWidth);
         }
     }
 }
