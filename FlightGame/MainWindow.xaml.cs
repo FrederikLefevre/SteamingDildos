94be9ce976d47ec7ab4132137664a54318479a6a
Frederik <frederik.lefevre@outlook.com>
2016-03-01 21:23:03 +0100

    Added enemy anim

--- FlightGame/MainWindow.xaml.cs
+++ FlightGame/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FlightGame
 {
@@ -12,6 +17,8 @@ namespace FlightGame
         //Fields
         //=======
         private bool _planeTaken = false;
+        DispatcherTimer _timer = new DispatcherTimer();
+        Random _rnd = new Random();
 
         //Constructors
         //=============
@@ -20,13 +27,47 @@ namespace FlightGame
             InitializeComponent();
         }
 
+        //Methods
+        //========
+        private void SpawnPlane()
+        {
+            TempPlane newPlane = new TempPlane();
+
+            double xPos = this._rnd.Next((int)this.Plane.Width, (int) this.PlayArea.ActualWidth);
+
+            Canvas.SetTop(newPlane, -newPlane.Height);
+            Canvas.SetLeft(newPlane, xPos);
+
+            this.PlayArea.Children.Add(newPlane);
+
+            double speed = this._rnd.Next(1, 10);
+
+            StartMoveDown(newPlane, newPlane.ActualHeight, TimeSpan.FromSeconds(speed));
+        }
+
+        private void StartMoveDown(UIElement element, double height, TimeSpan duration)
+        {
+            Storyboard story = new Storyboard();
+            DoubleAnimation anim = new DoubleAnimation()
+            {
+                From = 0,
+                To = this.PlayArea.ActualHeight + height,
+                Duration = duration
+            };
+
+            Storyboard.SetTarget(anim, element);
+            Storyboard.SetTargetProperty(anim, new PropertyPath("(Canvas.Top)"));
+
+            story.Children.Add(anim);
+            story.Begin();
+        }
+
         //Events
         //=======
         private void GrabPlane_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this._planeTaken = true;
         }
-
         private void MovePlane_OnMouseMove(object sender, MouseEventArgs e)
         {
             if (!_planeTaken)
@@ -50,6 +91,10 @@ namespace FlightGame
 
             Canvas.SetTop(this.Plane, startHeight);
             Canvas.SetLeft(this.Plane, startWidth);
+
+            this._timer.Interval = TimeSpan.FromSeconds(1.0);
+            this._timer.Tick += delegate { SpawnPlane(); };
+            this._timer.Start();
         }
     }
 }
